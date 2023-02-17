7bf1f38de0427034909032903aa2f4331638c22f
Julia Tortosa <julia.tortosa@ad.upcnet.es>
2023-02-17 14:00:12 +0100

    tut12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,8 @@ export class AppComponent {
     age: 26,
     avatar: 'https://www.w3schools.com/howto/img_avatar.png'
   }
+  names: string[] = ['Nico', 'Juli', 'Santi']
+  newName = '';
 
   toggleButton(){
     this.btnDisabled = !this.btnDisabled;
@@ -32,5 +34,16 @@ export class AppComponent {
   changeName(event: Event) {
     const element = event.target as HTMLInputElement;
     this.person.name = element.value;
+  }
+
+  addName() {
+
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+
+  deleteName(index: number) {
+
+    this.names.splice(index, 1);
   }
 }
