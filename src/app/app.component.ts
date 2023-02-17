3e1f1b3db8782095193e6b7c237a1865afa268f1
Julia Tortosa <julia.tortosa@ad.upcnet.es>
2023-02-17 11:55:44 +0100

    tut7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,10 @@ export class AppComponent {
   name = 'Júlia';
   age = 26;
   img = 'https://www.w3schools.com/howto/img_avatar.png';
+  btnDisabled = true;
+  person = {
+    name: 'Júlia',
+    age: 26,
+    avatar: 'https://www.w3schools.com/howto/img_avatar.png'
+  }
 }
