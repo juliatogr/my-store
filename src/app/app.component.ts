e723abadd7a3518d01075a32c4fd86f75c1ef596
Julia Tortosa <julia.tortosa@ad.upcnet.es>
2023-02-20 13:19:54 +0100

    curso2_tut4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,8 @@ import { Product } from './product.model';
 export class AppComponent {
   imgParent='';
   //imgParent='https://www.w3schools.com/howto/img_avatar.png';
+
+  onLoaded(img: string) {
+    console.log('log padre', img);
+  }
 }
