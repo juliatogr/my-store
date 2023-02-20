3e6963b5810d8f7fc4fbe37a0eeaccfbdc14bf3b
Julia Tortosa <julia.tortosa@ad.upcnet.es>
2023-02-20 13:43:53 +0100

    curso2_tut5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { Product } from './product.model';
+import { Product } from './models/product.model'
 
 @Component({
   selector: 'app-root',
@@ -10,6 +10,35 @@ import { Product } from './product.model';
 export class AppComponent {
   imgParent='';
   //imgParent='https://www.w3schools.com/howto/img_avatar.png';
+
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'Juguete',
+      image: './assets/images/toy.jpg',
+      price: 565
+    },
+    {
+      id: '2',
+      name: 'Libros',
+      image: './assets/images/books.jpg',
+      price: 565
+    },
+    {
+      id: '3',
+      name: 'Bici',
+      image: './assets/images/bike.jpg',
+      price: 565
+    },
+    {
+      id: '4',
+      name: 'Álbumes',
+      image: './assets/images/album.jpg',
+      price: 565
+    }
+
+
+  ];
 
   onLoaded(img: string) {
     console.log('log padre', img);
