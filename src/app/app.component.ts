5241f50eb6ff3fb7e721d61d460e47fa706b4acd
Julia Tortosa <julia.tortosa@ad.upcnet.es>
2023-02-20 09:17:21 +0100

    tut13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Product } from './product.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,6 +19,28 @@ export class AppComponent {
   }
   names: string[] = ['Nico', 'Juli', 'Santi']
   newName = '';
+  products: Product[] = [
+    {
+      name: 'El mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all'
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colección de albumes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    }
+  ]
 
   toggleButton(){
     this.btnDisabled = !this.btnDisabled;
