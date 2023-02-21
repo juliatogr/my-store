3d507a26307d2638db55f1419debd9451411476a
Julia Tortosa <julia.tortosa@ad.upcnet.es>
2023-02-21 12:54:08 +0100

    curso2_tut10

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../../models/product.model'
 
 @Component({
@@ -13,4 +13,10 @@ export class ProductComponent {
     image: '',
     name: ''
   };
+
+  @Output() addedProduct = new EventEmitter<Product>();
+
+  onAddToCart(){
+    this.addedProduct.emit(this.product);
+  }
 }
--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -7,18 +7,21 @@ import { Product } from 'src/app/models/product.model';
   styleUrls: ['./products-list.component.scss']
 })
 export class ProductsListComponent {
+  myShoppingCart: Product[] = [];
+  total = 0;
+
   products: Product[] = [
     {
       id: '1',
       name: 'Juguete',
       image: './assets/images/toy.jpg',
-      price: 565
+      price: 200
     },
     {
       id: '2',
       name: 'Libros',
       image: './assets/images/books.jpg',
-      price: 565
+      price: 50
     },
     {
       id: '3',
@@ -30,9 +33,13 @@ export class ProductsListComponent {
       id: '4',
       name: 'Álbumes',
       image: './assets/images/album.jpg',
-      price: 565
+      price: 125
     }
-
-
   ];
+
+  onAddToShoppingCart(product: Product){
+    this.myShoppingCart.push(product);
+    this.total = this.myShoppingCart.reduce((sum, item) => sum + item.price, 0);
+    console.log(product);
+  }
 }
