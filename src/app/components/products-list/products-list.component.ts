6b447dec36406631f878dc8caceae1c483ee967c
Julia Tortosa <julia.tortosa@ad.upcnet.es>
2023-02-22 13:27:50 +0100

    curso3tut8

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -30,6 +30,9 @@ export class ProductsListComponent implements OnInit {
     description: ''
   };
 
+  limit = 10;
+  offset = 0;
+
   constructor(
     private storeService: StoreService,
     private productsService: ProductsService
@@ -38,10 +41,7 @@ export class ProductsListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.productsService.getAllProducts()
-    .subscribe(data => {
-      this.products = data;
-    })
+    this.loadMore();
   }
 
   onAddToShoppingCart(product: Product){
@@ -98,6 +98,14 @@ export class ProductsListComponent implements OnInit {
       const productIndex = this.products.findIndex(item => item.id === this.productChosen.id);
       this.products.splice(productIndex, 1);
       this.showProductDetail = false;
+    })
+  }
+
+  loadMore() {
+    this.productsService.getAllProducts(this.limit, this.offset)
+    .subscribe(data => {
+      this.products = this.products.concat(data);
+      this.offset += this.limit;
     })
   }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { CreateProductDTO, Product, UpdateProductDTO } from '../models/product.model';
 
 @Injectable({
@@ -13,8 +13,13 @@ export class ProductsService {
     private http: HttpClient
   ) { }
 
-  getAllProducts() {
-    return this.http.get<Product[]>(this.apiUrl);
+  getAllProducts(limit?: number, offset?: number) {
+    let params = new HttpParams();
+    if (limit != undefined && offset != undefined) {
+      params = params.set('limit', limit);
+      params = params.set('offset', offset);
+    }
+    return this.http.get<Product[]>(`${this.apiUrl}`, { params })
   }
 
   getProduct(id: string) {
