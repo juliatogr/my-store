ce116a803419ed721e732a05fe059b5a9a6fdaa4
Julia Tortosa <julia.tortosa@ad.upcnet.es>
2023-02-23 12:00:12 +0100

    curso3_tut16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +12,34 @@ export class AppComponent {
   //imgParent='https://www.w3schools.com/howto/img_avatar.png';
   showImg = true;
 
+  constructor (
+    private authService: AuthService,
+    private usersService: UsersService
+  ) {}
+
   onLoaded(img: string) {
     console.log('log padre', img);
   }
 
   toggleImg() {
     this.showImg = !this.showImg;
+  }
+
+  createUser() {
+    this.usersService.create({
+      name: 'Sebas',
+      email: 'sebas@mail.com',
+      password: '1212'
+    })
+    .subscribe(rta => {
+      console.log(rta);
+    })
+  }
+
+  login() {
+    this.authService.login('sebas@mail.com', '1212')
+    .subscribe(rta => {
+      console.log(rta.access_token);
+    })
   }
 }
