8e27636a730cb4be19e0c3b19ed0aab42177c68a
Julia Tortosa <julia.tortosa@ad.upcnet.es>
2023-02-23 09:37:55 +0100

    curso3_tut12

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { CreateProductDTO, Product, UpdateProductDTO } from '../models/product.model';
 import { retry } from 'rxjs/operators';
+import { environment } from './../../environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  private apiUrl = '/api/products';
+  private apiUrl = `${environment.API_URL}/api/products`;
 
   constructor(
     private http: HttpClient
