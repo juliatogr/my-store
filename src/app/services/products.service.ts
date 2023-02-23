877c78125c32cdc5d4d8276b128c5ff6572f471e
Julia Tortosa <julia.tortosa@ad.upcnet.es>
2023-02-23 10:56:22 +0100

    curso3_tut14

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
 import { CreateProductDTO, Product, UpdateProductDTO } from '../models/product.model';
-import { retry, catchError } from 'rxjs/operators';
+import { retry, catchError, map } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import { environment } from './../../environments/environment'
 
@@ -24,7 +24,13 @@ export class ProductsService {
     }
     return this.http.get<Product[]>(`${this.apiUrl}`, { params })
     .pipe(
-      retry(3)
+      retry(3),
+      map(products => products.map(item => {
+        return {
+          ...item,
+          taxes: .19 * item.price
+        }
+      }))
     );
   }
 
