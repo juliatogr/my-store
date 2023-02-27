3e83f3761f91832b21ed08479bb0b7f6e03f9279
Julia Tortosa <julia.tortosa@ad.upcnet.es>
2023-02-27 09:32:00 +0100

    curso4_tut8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { RecoveryComponent } from './pages/recovery/recovery.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 
 const routes: Routes = [
   {
@@ -21,12 +22,11 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
-    path: 'notfound',
-    component: NotFoundComponent
-  },
-  {
     path: 'category/:id',
     component: CategoryComponent
+  }, {
+    path: 'product/:id',
+    component: ProductDetailComponent
   },
   {
     path: 'my-cart',
@@ -51,6 +51,9 @@ const routes: Routes = [
   {
     path: 'profile',
     component: ProfileComponent
+  }, {
+    path: '**',
+    component: NotFoundComponent
   }
 ];
 
