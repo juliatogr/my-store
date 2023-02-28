703dfee9cccff04d46bf12f200336919446e0d08
Julia Tortosa <julia.tortosa@ad.upcnet.es>
2023-02-28 12:08:48 +0100

    curso4_tut23

--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -11,6 +11,7 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 
 import { AuthGuard } from "../guards/auth.guard";
+import { ExitGuard } from "../guards/exit.guard";
 
 const routes: Routes = [
   {
@@ -49,6 +50,7 @@ const routes: Routes = [
       ,
       {
         path: 'register',
+        canDeactivate: [ ExitGuard ],
         component: RegisterComponent
       }
       ,
